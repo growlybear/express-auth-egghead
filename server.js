55e86056ccbdfb43ee5f2881acfc6141271d3513
Michael Allan <Michael Allan>
2018-07-03 16:05:25 +1000

    Handle POST to /login

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 const PORT = process.env.PORT || 4000
 
+// middlewares
+app.use(bodyParser.json())
+
 app.get('/status', (req, res) => {
   const localTime = (new Date()).toLocaleString()
   res.status(200).send(`Server time is ${localTime}`)
+})
+
+app.post('/login', (req, res) => {
+  const user = req.body.username
+  res.status(200).send(`User ${user} attempting to login`)
 })
 
 app.get('*', (req, res) => {
