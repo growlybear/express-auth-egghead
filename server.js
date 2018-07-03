78bea5e8be68f11892dfbd78bc0077df4f336368
Michael Allan <Michael Allan>
2018-07-03 16:28:49 +1000

    Require username/password on /login, create jwt onSuccess

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken')
 
 const app = express()
 const PORT = process.env.PORT || 4000
+
+// users 'db' ☠️
+const users = [
+  { id: 1, username: "admin", password: "admin" },
+  { id: 2, username: "guest", password: "guest" },
+]
 
 // middlewares
 app.use(bodyParser.json())
@@ -14,7 +21,21 @@ app.get('/status', (req, res) => {
 
 app.post('/login', (req, res) => {
   const user = req.body.username
-  res.status(200).send(`User ${user} attempting to login`)
+  const pass = req.body.password
+  const found = users.find(u => u.username === user && u.password === pass)
+  if (!user || !pass) {
+    res.status(400).end(`Username and password required`)
+  }
+  if (!found) {
+    res.status(401).end(`User not found`)
+  }
+  const token = jwt.sign({
+    sub: found.id,
+    username: found.username
+  }, 'mysupersecretkeythingy', {
+    expiresIn: '3 hours'
+  })
+  res.status(200).send({ access_token: token })
 })
 
 app.get('*', (req, res) => {
