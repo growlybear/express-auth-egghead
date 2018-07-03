68f07a50b2724ce868505975053d829574e3f32c
Michael Allan <Michael Allan>
2018-07-03 15:36:06 +1000

    Initial server implementation

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express()
+const PORT = 4000
+
+app.get('/status', (req, res) => {
+  const localTime = (new Date()).toLocaleString()
+  res.status(200).send(`Server time is ${localTime}`)
+})
+
+app.get('*', (req, res) => {
+  res.sendStatus(404)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`)
+})
